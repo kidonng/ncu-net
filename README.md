4a0967f5f89f676eb5bbf761090e95750528376d
Kid <kidonng@gmail.com>
2019-05-01 11:18:32 +0800

    Mutil language & alternative check

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # NCU Net [![](https://img.shields.io/badge/Standalone-blue.svg)](https://github.com/kidonng/cherry#user-content-ncu-net-)
 
-Auto login NCU Campus Network Access Authentication System.
+**UNDER DEVELOPMENT**
 
-Note: Differing from the user script, currently it only works on `NCU-5G/NCU-2.4G`.
+Auto login NCU Campus Network Access Authentication System with auto reconnect.
+
+Note: Currently only support `NCU-5G/NCU-2.4G` login (no logout).
 
 ## Usage
 
 1. Run `npm i` to setup.
-2. [Config your username & password](ncu-net.js#L10-L15).
+2. [Config username, password & more](ncu-net.js#L1-L7).
 3. Run `npm start/stop` to start/stop the daemon.
--- ncu-net.js
+++ ncu-net.js
@@ -1,23 +1,47 @@
-const got = require('got')
-const Hashes = require('./lib/hashes.min')
-
-const log = (color, msg) =>
-  console.log(
-    require('chalk')[color](`${new Date().toTimeString().slice(0, 8)} ${msg}`)
-  )
-const callback = data => data
-
 const config = {
   username: '',
   password: '',
-  checkInterval: 3000,
-  retryInterval: 10000
+  lang: 'en', // Available language: en (English) / zh (Simplified Chinese)
+  checkInterval: 5000,
+  retryInterval: 10000 // Recommend >= 10s
 }
 
-;(async () => {
-  log('blue', 'Connecting...')
+const msg =
+  config.lang === 'zh'
+    ? {
+        connecting: '正在连接……',
+        connectSuccess: '连接成功。',
+        connectFailed: `连接失败！${config.retryInterval /
+          1000} 秒后重试，点击注销按钮取消。`,
+        connectError: '连接异常！正在重新连接……'
+      }
+    : {
+        connecting: 'Connecting...',
+        connectSuccess: 'Connect success.',
+        connectFailed: `Connect failed! Retry in ${config.retryInterval /
+          1000} sec(s), click logout button to cancel.`,
+        connectError: 'Connect error! Reconnecting...'
+      }
 
-  const $ = require('cheerio').load((await got('http://222.204.3.154/')).body)
+const chalk = require('chalk')
+const cheerio = require('cheerio')
+const got = require('got')
+const Hashes = require('./lib/hashes.min')
+
+const log = (color, msg) => {
+  const now = new Date()
+  console.log(
+    chalk[color](
+      `${now.toLocaleDateString()} ${new Date().toLocaleTimeString()} ${msg}`
+    )
+  )
+}
+const callback = data => data
+
+;(async () => {
+  log('blue', msg.connecting)
+
+  const $ = cheerio.load((await got('http://222.204.3.154/')).body)
   const ip = $('[name="user_ip"]').val()
   const ac_id = $('[name="ac_id"]').val()
   const n = 200
@@ -69,24 +93,38 @@ const config = {
 
     // E2620: Already connected
     if (res.res === 'ok' || res.ecode === 'E2620') {
-      log('green', 'Connect success.')
+      log('green', msg.connectSuccess)
       timer = setInterval(check, config.checkInterval)
     } else {
-      log(
-        'red',
-        `Connect failed! Retry in ${config.retryInterval / 1000} sec(s).`
-      )
+      log('red', msg.connectFailed)
       timer = setTimeout(connect, config.retryInterval)
     }
   }
 
   const check = async () => {
-    if (
-      (await got('http://222.204.3.154/cgi-bin/rad_user_info')).body.indexOf(
-        'not_online'
-      ) === 0
-    ) {
-      log('red', 'Connect error! Reconnecting...')
+    try {
+      if (
+        (await got('http://222.204.3.154/cgi-bin/rad_user_info')).body.includes(
+          'not_online_error'
+        )
+      ) {
+        log('red', msg.connectError)
+        clearInterval(timer)
+        connect()
+      }
+    } catch {
+      clearInterval(timer)
+      timer = setTimeout(alternativeCheck, config.checkInterval)
+    }
+  }
+
+  const alternativeCheck = async () => {
+    try {
+      await got(
+        `https://i.loli.net/2019/04/28/5cc55262e0b92.png?${Math.random()}`
+      )
+    } catch {
+      log('red', msg.connectError)
       clearInterval(timer)
       connect()
     }
